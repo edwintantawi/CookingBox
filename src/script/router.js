2156a6a73354ce9b7fd957072bab995af0748990
edwintantawi <edwintantawi@gmail.com>
2021-01-17 17:13:05 +0700

    added search section and cards

--- src/script/router.js
+++ src/script/router.js
@@ -7,16 +7,15 @@ renderPage(route);
 
 export function renderPage(route){
   const xhr = new XMLHttpRequest();
-  console.log("ok")
   xhr.onreadystatechange = function(){
 
     if( this.readyState === 4 ){
       if( this.status === 200 ){
         root.innerHTML = this.responseText;
       } else if( this.status === 404 ){
-        root.innerHTML = "<p>ERROR 404</p>"
+        root.innerHTML = "<h1><center>ERROR 404 Page Not Found</center></h1>"
       } else {
-        root.innerHTML = "<p>Opss.. Somethings Wrong</p>"
+        root.innerHTML = "<h1><center>Opss.. Somethings Wrong</center></h1>"
       }
     }
   }
